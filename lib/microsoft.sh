95e6fb3527dec3dba3a807134f6cecd68e714fbc
yishino <yishino@jscom.co.jp>
2021-06-04 14:33:22 +0900

    Add alias for Windows.

--- lib/microsoft.sh
+++ lib/microsoft.sh
@@ -18,14 +18,14 @@ _create_home() {
 
 _create_symlinks() {
   func_msg title '[INFO]: Create symlinks for '$OS'.'
-  ln -snf $ROOT_PATH/AppData/Roaming/Microsoft/Windows/Start\ Menu/Programs/Startup $HOME/startup
+  ln -snf $ROOT_PATH/AppData/Roaming/Microsoft/Windows/Start\ Menu/Programs/Startup $HOME/startup &&
   func_msg success 'Startup'
-  ln -snf $ROOT_PATH/Desktop $HOME/desktop
+  ln -snf $ROOT_PATH/Desktop $HOME/desktop &&
   func_msg success 'Desktop'
-  ln -snf $ROOT_PATH/Downloads $HOME/downloads
+  ln -snf $ROOT_PATH/Downloads $HOME/downloads &&
   func_msg success 'Downloads'
   [ ! -e $ROOT_PATH/Documents/work ] && mkdir $ROOT_PATH/Documents/work
-  ln -snf $ROOT_PATH/Documents/work $HOME/work
+  ln -snf $ROOT_PATH/Documents/work $HOME/work &&
   func_msg success 'Documents/work'
 }
 
